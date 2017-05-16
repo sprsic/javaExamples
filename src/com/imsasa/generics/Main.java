236e187d697ba2d83069203f3e9d5a208a372ab9
Sasa Prsic <s.prsic@gmail.com>
2017-05-16 21:50:27 +0200

    Added generic max method

--- src/com/imsasa/generics/Main.java
+++ src/com/imsasa/generics/Main.java
@@ -1,5 +1,8 @@
 package com.imsasa.generics;
 
+import java.util.Iterator;
+import java.util.List;
+
 public class Main {
 
     private static UnaryFunction<Object> IDENTITY_FUNCTION = arg -> arg; // return same element - F(x) = x - identity function
@@ -16,6 +19,19 @@ public class Main {
         UnaryFunction<Number> identityNumber = identityFunction();
         for (Number n : numbers)
             System.out.println(identityNumber.applyFunction(n));
+    }
+
+
+    public static <T extends Comparable<T>> T max(List<T> list) {
+
+        Iterator<T> i = list.iterator();
+        T result = i.next();
+        while (i.hasNext()) {
+            T t = i.next();
+            if (t.compareTo(result) > 0)
+                result = t;
+        }
+        return result;
 
     }
 
